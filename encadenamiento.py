20e8faea09064f97227355391ee6ba8f6d47e4d9
Alvaro Davila Gomez <alvaritodavilag0331@gmail.com>
2025-02-03 11:37:20 -0600

    defino el valor de los nodos

---
+++ encadenamiento.py
@@ -0,0 +1,21 @@
+
+
+
+
+#Definir valores de los nodos
+nodos={
+    "A": None,
+    "B":None,
+    "C":None,
+    "D":True,
+    "E":True,
+    "F":True,
+    "G":None,
+    "H":None,
+    "I":None,
+    "J":None,
+    "K":None,
+    "L":True,
+    "M":None
+}
+
